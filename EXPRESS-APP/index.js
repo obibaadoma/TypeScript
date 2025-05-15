faa51c5fa2006a0cf9ed90a71cc51d344c0e7bb9
Daa Nkwa <akrasikonadu@yahoo.com>
2025-05-15 14:35:30 +0000

    add delete

--- EXPRESS-APP/index.js
+++ EXPRESS-APP/index.js
@@ -4,6 +4,8 @@ import router from './route.js';
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
 
 // DEFINE a simple route
@@ -17,6 +19,30 @@ app.get('search', searchController);
 // query a string
 
 app.use('/user', router);
+
+app.use(express.json());
+
+app.post('/users', (req, res)=>{
+  const {name, email} = req.body
+  res.json({
+    message: `User ${name} with email ${email} created successfully`
+  })
+})
+
+app.put('/users/:id', (req, res)=>{
+  const userId = req.params.id;
+  const {name, email} = req.body
+  res.json({
+    message: `User ${userId} updated to${name}, ${email}`
+  })
+})
+
+app.delete('/users/:id', (req, res)=>{
+  const userId = req.params.id;
+  res.json({
+    message: `User ${userId} deleted successfully`
+  })
+})
 
 app.listen(PORT, ()=>{
   console.log(`Server is running on http://localhost:${PORT}`);
