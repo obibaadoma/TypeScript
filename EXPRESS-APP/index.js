2df6e156b890ea2704cb019f226888c8b4a684d6
Daa Nkwa <akrasikonadu@yahoo.com>
2025-05-15 00:48:24 +0000

    installed nodemon

--- EXPRESS-APP/index.js
+++ EXPRESS-APP/index.js
@@ -7,5 +7,9 @@ const PORT = 3000;
 // DEFINE a simple route
 
 app.get('/', (req, res) => {
-  
-}
+  res.send('Hello Express');
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
